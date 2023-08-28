edf8f1beb28092492e90061beb7dac91e0b388f8
Victor Cubas <vhcubas91@gmail.com>
2023-08-27 20:22:50 -0400

    Se agrega los primeros componentes para implementar redux en el modulo de inscripciones

--- src/app/dashboard/pages/inscripciones/inscripciones.module.ts
+++ src/app/dashboard/pages/inscripciones/inscripciones.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { InscripcionesComponent } from './inscripciones/inscripciones.component';
 import { InscripcionesRoutingModule } from './inscripciones/inscripciones-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { EffectsModule } from '@ngrx/effects';
+import { InscripcionesEffects } from './store/inscripciones.effects';
+import { inscripcionesFeature } from './store/inscripciones.reducer';
+import { StoreModule } from '@ngrx/store';
 
 
 
@@ -13,7 +17,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     InscripcionesRoutingModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forFeature(inscripcionesFeature),
+    EffectsModule.forFeature([InscripcionesEffects])
   ]
 })
 export class InscripcionesModule { }
--- src/app/dashboard/pages/inscripciones/inscripciones/inscripciones.component.ts
+++ src/app/dashboard/pages/inscripciones/inscripciones/inscripciones.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-inscripciones',
   templateUrl: './inscripciones.component.html',
   styleUrls: ['./inscripciones.component.scss']
 })
-export class InscripcionesComponent {
-  displayedColumns = ['id', 'name', 'email'];
+export class InscripcionesComponent implements OnInit{
+  displayedColumns = ['id', 'name', 'course', 'total'];
+
+  constructor(private store: Store){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
